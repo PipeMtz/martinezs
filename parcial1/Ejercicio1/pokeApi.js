73d12145bc511718fa89df1bcdc4b66b0d2d74ea
PipeMtz <felipilloto2@gmail.com>
2023-08-22 08:39:39 -0500

    Ejercicio 2 22-Aug

--- parcial1/Ejercicio1/pokeApi.js
+++ parcial1/Ejercicio1/pokeApi.js
@@ -5,16 +5,24 @@
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
 
-const randomNumber = Math.floor(Math.random() * 200) + 1;
+function fetchPokemon(){
+        const randomNumber = Math.floor(Math.random() * 200) + 1;
+        
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+        
+        const options = { method: 'GET' };
+        
+                fetch(apiUrl, options)
+                    .then(response => response.json())
+                    .then(data => {
+                        const pokemonName = data.name;
+                        const pokemonWeight = data.weight;
+                        const pokemonTypes = data.types.map(type => type.type.name).join(', ');
+                        document.getElementById('pokemonName').textContent = pokemonName;
+                        document.getElementById('pokemonWeight').textContent = (pokemonWeight) + " kg";
+                        document.getElementById('pokemonTypes').textContent = pokemonTypes;
+                    })
+                    .catch(err => console.error(err));   
+}
 
-const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-
-const options = { method: 'GET' };
-
-        fetch(apiUrl, options)
-            .then(response => response.json())
-            .then(data => {
-                const pokemonName = data.name;
-                document.getElementById('pokemonName').textContent = pokemonName;
-            })
-            .catch(err => console.error(err));
+document.getElementById('fetchBtn').addEventListener('click',fetchPokemon)
