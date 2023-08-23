d430a571e34f13e5f07001551cf2b32c27bff659
PipeMtz <felipilloto2@gmail.com>
2023-08-23 08:44:41 -0500

    Ejercicio 23-Aug Multiples Fetch

--- parcial1/Ejercicio1/pokeApi.js
+++ parcial1/Ejercicio1/pokeApi.js
@@ -5,7 +5,38 @@
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
 
-function fetchPokemon(){
+function fetchXML(){
+
+    
+    
+    const randomNumber = Math.floor(Math.random() * 200) + 1;
+    
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+    
+    const xhr = new XMLHttpRequest()
+    {
+        xhr.open('GET', apiUrl, true);
+        xhr.onreadystatechange = ()=>{
+            if (xhr.readyState ===4 && xhr.status == 200){
+                var data = JSON.parse(xhr.responseText);
+
+                const pokemonName = data.name;
+                        const pokemonWeight = data.weight;
+                        const pokemonTypes = data.types.map(type => type.type.name).join(', ');
+                        const pokemonImage = data.sprites.front_default;
+                        document.getElementById('pokemonName').textContent = pokemonName;
+                        document.getElementById('pokemonWeight').textContent = (pokemonWeight) + " lb";
+                        document.getElementById('pokemonTypes').textContent = pokemonTypes;
+                        document.getElementById('image').src = pokemonImage;
+            }
+        };
+        xhr.send();
+    }
+
+
+}
+
+function fetchPromise(){
         const randomNumber = Math.floor(Math.random() * 200) + 1;
         
         const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
@@ -27,4 +58,73 @@ function fetchPokemon(){
                     .catch(err => console.error(err));   
 }
 
-document.getElementById('fetchBtn').addEventListener('click',fetchPokemon)
+async function fetchAsync(){
+    const randomNumber = Math.floor(Math.random() * 200) + 1;
+        
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+        
+    try {
+        const response = await fetch(apiUrl);
+
+        const data = await response.json();
+            const pokemonName = data.name;
+            const pokemonWeight = data.weight;
+            const pokemonTypes = data.types.map(type => type.type.name).join(', ');
+            const pokemonImage = data.sprites.front_default;
+            document.getElementById('pokemonName').textContent = pokemonName;
+            document.getElementById('pokemonWeight').textContent = (pokemonWeight) + " lb";
+            document.getElementById('pokemonTypes').textContent = pokemonTypes;
+            document.getElementById('image').src = pokemonImage;
+                      
+    } catch (error) {
+        
+    }
+                
+}
+
+function fetchJQUERY(){
+    const randomNumber = Math.floor(Math.random() * 200) + 1;
+        
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+    $.ajax({
+        url:apiUrl,
+        method:"GET",
+        success:(data)=>{ 
+            const pokemonName = data.name;
+            const pokemonWeight = data.weight;
+            const pokemonTypes = data.types.map(type => type.type.name).join(', ');
+            const pokemonImage = data.sprites.front_default;
+            document.getElementById('pokemonName').textContent = pokemonName;
+            document.getElementById('pokemonWeight').textContent = (pokemonWeight) + " lb";
+            document.getElementById('pokemonTypes').textContent = pokemonTypes;
+            document.getElementById('image').src = pokemonImage;
+         },
+        error :(e)=>{}
+        
+    })
+}
+
+function fetchAxios(){
+    const randomNumber = Math.floor(Math.random() * 200) + 1;
+        
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+    axios(apiUrl).then((res)=>{
+            const data = res.data;
+            const pokemonName = data.name;
+            const pokemonWeight = data.weight;
+            const pokemonTypes = data.types.map(type => type.type.name).join(', ');
+            const pokemonImage = data.sprites.front_default;
+            document.getElementById('pokemonName').textContent = pokemonName;
+            document.getElementById('pokemonWeight').textContent = (pokemonWeight) + " lb";
+            document.getElementById('pokemonTypes').textContent = pokemonTypes;
+            document.getElementById('image').src = pokemonImage;
+    }).catch(error =>{})
+}
+
+
+
+document.getElementById('fetchXML').addEventListener('click',fetchXML)
+document.getElementById('fetchPromise').addEventListener('click',fetchPromise)
+document.getElementById('fetchAsync').addEventListener('click',fetchAsync)
+document.getElementById('fetchJQUERY').addEventListener('click',fetchJQUERY)
+document.getElementById('fetchAxios').addEventListener('click',fetchAxios)
