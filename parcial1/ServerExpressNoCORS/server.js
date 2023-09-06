6c9b1da97020b2c0dc70cb63e6439d8642842888
PipeMtz <felipilloto2@gmail.com>
2023-09-06 08:46:43 -0500

    ServerLog Morgan

--- parcial1/ServerExpressNoCORS/server.js
+++ parcial1/ServerExpressNoCORS/server.js
@@ -2,22 +2,18 @@ import express from "express";
 
 const server = express();
 
+server.use(express.json());
 
-server.use(express.json())
+server.get("/alumnos/:nombre", (req, res) => {
+  res.send("Ruta de nombres, esta peticion evita CORS");
+});
 
-server.get("/alumnos/:nombre",(req,res)=>{
-  res.send("Ruta de nombres, esta peticion evita CORS")
-})
-
-
-
- server.post("/alumnos", (req, res) => {
+server
+  .post("/alumnos", (req, res) => {
     //res.send("Servidor express contestando GET");
-    const {nombre} = req.body
-    res.send(nombre)
+    const { nombre } = req.body;
+    res.send(nombre);
   })
   .listen(8000, (req, res) => {
     console.log("El servidor express escuchando");
   });
-
-
